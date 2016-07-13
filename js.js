403dd9ee39f761fcd9ec95cc71f5d78579c5fec7
Rokyzz <rokas90@gmail.com>
2016-07-14 00:29:19 +0300

    JS selector and TREE

--- js.js
+++ js.js
@@ -37,7 +37,7 @@ function treeButton(){
 	star.innerHTML = " ";
 	if (1 >= sum || sum >= 51){
 		star.innerHTML = star.innerHTML + "Wrong number. Please add number form 2 to 50";
-								}		
+		}		
 	else {
 	for (i = 0; i < sum; i++){
 		for (c = 0; c < (sum - i); c++){
@@ -47,6 +47,6 @@ function treeButton(){
 			star.innerHTML = star.innerHTML + "*";
 		}
 		star.innerHTML = star.innerHTML + "<br>";
-							}
 		}
-					}
+		}
+}
